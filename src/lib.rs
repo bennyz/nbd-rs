579bc3d15f5ee3dacb46dbaaa05c0e0bba0fa12f
Benny Zlotnik <bzlotnik@protonmail.com>
2022-10-10 14:20:50 +0300

    start client negotiation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod consts;
-mod frame;
+mod handshake;
 pub mod server;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,8 @@
-use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tokio::net::ToSocketAddrs;
 
-use crate::frame::Frame;
-use crate::frame::HandshakeFlags;
+use crate::handshake::connection::Connection;
 
 pub struct Server {
     listener: TcpListener,
@@ -20,6 +18,8 @@ impl Server {
         loop {
             let (socket, addr) = self.listener.accept().await?;
 
+            println!("Received connection from {addr}");
+
             // If TCP sockets are used, both the client and server SHOULD disable Nagle's algorithm
             // (that is, use setsockopt to set the TCP_NODELAY option to non-zero)
             // https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md#protocol-phases
@@ -33,31 +33,6 @@ impl Server {
     async fn process(&self, socket: TcpStream) -> anyhow::Result<()> {
         let conn = Connection::new(socket);
         conn.handshake().await?;
-
-        Ok(())
-    }
-}
-
-pub struct Connection {
-    conn: TcpStream,
-}
-
-impl Connection {
-    pub fn new(conn: TcpStream) -> Self {
-        Self { conn }
-    }
-
-    pub async fn handshake(mut self) -> anyhow::Result<()> {
-        let handshake_flags = HandshakeFlags::FIXED_NEWSTYLE | HandshakeFlags::NO_ZEROES;
-        self.send_frame(Frame::ServerHandshake(handshake_flags))
-            .await?;
-
-        Ok(())
-    }
-
-    pub async fn send_frame(&mut self, frame: Frame) -> anyhow::Result<()> {
-        frame.write(&mut self.conn).await?;
-        self.conn.flush().await?;
 
         Ok(())
     }
